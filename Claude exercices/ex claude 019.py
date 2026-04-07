341da648fbf3cab625d310f59fb2c32fd4578f03
FernandesTiago <castelantiago@gmaill.com>
2026-04-07 20:50:53 -0300

    learned json, did more exercices

--- Claude exercices/ex claude 019.py
+++ Claude exercices/ex claude 019.py
@@ -11,6 +11,16 @@ def salvar_dados(dados):
     with open('dados19.json','w') as arquivo:
         json.dump(dados, arquivo)
 
+def deletar_dados():
+    """DELETA A LISTA COMPLETA"""
+    with open('dados19.json','w'):
+        pass
+
+def printar(txt):
+    """PRINTA A LISTA DE MANEIRA ORGANIZADO"""
+    texto = sorted(txt, key=lambda x: x['pontuacao'], reverse=True)
+    for i in range(0, len(texto)):
+        print(f'Nome: {texto[i]['nome']:<10} Pontuação: {texto[i]['pontuacao']}')
 
 placar = [
     {'nome':'Mocotó', 'pontuacao': 2000},
@@ -22,12 +32,33 @@ placar = [
 while True:
     try:
         texto = carregar_dados()
-        if texto:
-            print('\nTabela do torneio\n')
-            for i in range(0, len(texto)):
-                print(f'Nome: {texto[i]['nome']:<10} Pontuação: {texto[i]['pontuacao']}')
-            break
-        else:
-            salvar_dados(placar)
-    except FileNotFoundError:
+        print('\nTabela do torneio\n')
+        printar(texto)
+        break
+    except (FileNotFoundError, json.JSONDecodeError):
         salvar_dados(placar)
+
+while True:
+    adicionar = input('Deseja adicionar um novo jogador? (S/N/D) ').strip().title()
+    if adicionar == 'S':
+        jogador = input('Qual o nome do jogador? ').title().strip()
+        while True:
+            try:
+                pontuacao = int(input('Qual a pontuacao? '))
+                break
+            except ValueError:
+                print('Digite um numero inteiro!')
+        dic_jogador = {'nome': jogador, 'pontuacao': pontuacao}
+        texto.append(dic_jogador)
+        print('\nTabela do torneio\n')
+        printar(texto)
+        salvar_dados(texto)
+        break
+    elif adicionar == 'D':
+        deletar_dados()
+        break
+    elif adicionar == 'N':
+        print('Nada adicionado!')
+        break
+    else:
+        print('Digite S, N ou D!!')
